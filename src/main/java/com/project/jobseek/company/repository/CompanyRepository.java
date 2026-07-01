0f5887db034c494357c4c89ffa30ab182a513ffc
danielprinceD <d.danielprince2003@gmail.com>
2026-07-01 22:56:18 +0530

    company entity server startup failure

--- src/main/java/com/project/jobseek/company/repository/CompanyRepository.java
+++ src/main/java/com/project/jobseek/company/repository/CompanyRepository.java
@@ -1,10 +1,13 @@
 package com.project.jobseek.company.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.project.jobseek.company.model.Company;
 
-public abstract class CompanyRepository implements JpaRepository<Company, Long>
+@Service
+public interface CompanyRepository extends JpaRepository<Company, Long>
 {
 
 }
