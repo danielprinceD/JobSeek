7f2551a0ca6b56b43789dbe4f2386b77e0573dc3
danielprinceD <d.danielprince2003@gmail.com>
2026-07-01 21:19:20 +0530

    inital commit
    # Conflicts:
    #	README.md

---
+++ src/main/java/com/project/JobSeek/JobSeekApplication.java
@@ -0,0 +1,12 @@
+package com.project.JobSeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekApplication.class, args);
+	}
+
+}
